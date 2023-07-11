8905e20034ba32d954c307d61841e5fe6c5ff4ad
Anirudra-Banerjee <anibanerjee200@gmail.com>
2023-07-11 20:42:17 +0530

    first commit

--- PalindromeLinkedListCheck.java
+++ PalindromeLinkedListCheck.java
@@ -5,33 +5,32 @@ class PalindromeLinkedList{
     public static void main(String args[]){
 
         Scanner sc = new Scanner(System.in);
-        LinkedList<Integer> list = new LinkedList<Integer>();
+        LinkedList<String> list = new LinkedList<String>();
         System.out.println("Enter the no of elements: ");
         int n = sc.nextInt();
+        
         for(int i = 0; i < n; i++){
-            list.add(sc.nextInt());
+            list.add(sc.nextLine());
         }
 
         System.out.println(list);
 
         boolean flag = true;
         for(int i = 0; i < n / 2; i++){
-            if(list.get(i) != list.get(n - 1 - i)){
-                System.out.println("Not Plindrome");
-                flag = false;
-                break;
+            if(list.get(i).equals(list.get(n - 1 - i)) ){
+                
             }
-            
+            else{
+                flag = false;
+            }
         }
         if(flag){
             System.out.println("The linked list is Palindrome.");
         }
+        else{
+            System.out.println("Not Palindrome");
+        }
 
     }
     
-    // while(list.Reverse()){
-
-    // }
-
-
 }
